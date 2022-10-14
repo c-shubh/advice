f28417168a5c3e57bcb49f20ff2fba781c13b955
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-10-14 16:12:56 +0530

    fix: disallow fetch caching

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // References
 let textArea = document.querySelector(".text-area");
 
-fetch("https://api.adviceslip.com/advice")
+fetch("https://api.adviceslip.com/advice?", {
+    cache: "no-store",
+})
     .then((response) => response.json())
     .then((slipObj) => (textArea.innerText = slipObj.slip.advice));
